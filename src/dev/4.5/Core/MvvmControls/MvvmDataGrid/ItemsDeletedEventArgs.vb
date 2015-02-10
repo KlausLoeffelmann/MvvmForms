e76192336998eccadac5a19e3ef5560c41288f41
helwinter <stephan.urgien@activedevelop.de>
2015-02-10 17:12:57 +0100

    EventArgs für MvvmDataGrid hinzugefügt

---
+++ src/dev/4.5/Core/MvvmControls/MvvmDataGrid/ItemsDeletedEventArgs.vb
@@ -0,0 +1,10 @@
+﻿
+Public Class ItemsDeletedEventArgs
+    ''' <summary>
+    ''' Die vom DataGrid geleoschten Items
+    ''' </summary>
+    ''' <value></value>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Property DeletedItems As IEnumerable
+End Class
