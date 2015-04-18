33ea2f5724119ba1e899abef159ca6bb2798ebe2
vbspoony <klaus@loeffelmann.de>
2015-04-19 00:13:43 +0200

    Eigentliche Demo sowie Solution Git hinzugefuegt.

---
+++ src/dev/4.5/Samples/MeterReaderMvvmForms/MeterReaderMvvmForms/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewReadingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewContactToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewBuildingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.QuitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditContactToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditBuildingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(712, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewReadingToolStripMenuItem, Me.NewContactToolStripMenuItem, Me.NewBuildingToolStripMenuItem, Me.ToolStripMenuItem1, Me.QuitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'NewReadingToolStripMenuItem
+        '
+        Me.NewReadingToolStripMenuItem.Name = "NewReadingToolStripMenuItem"
+        Me.NewReadingToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.NewReadingToolStripMenuItem.Text = "New Reading..."
+        '
+        'NewContactToolStripMenuItem
+        '
+        Me.NewContactToolStripMenuItem.Name = "NewContactToolStripMenuItem"
+        Me.NewContactToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.NewContactToolStripMenuItem.Text = "New Contact..."
+        '
+        'NewBuildingToolStripMenuItem
+        '
+        Me.NewBuildingToolStripMenuItem.Name = "NewBuildingToolStripMenuItem"
+        Me.NewBuildingToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.NewBuildingToolStripMenuItem.Text = "New Building..."
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(151, 6)
+        '
+        'QuitToolStripMenuItem
+        '
+        Me.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem"
+        Me.QuitToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.QuitToolStripMenuItem.Text = "Quit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EditContactToolStripMenuItem, Me.EditBuildingToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'EditContactToolStripMenuItem
+        '
+        Me.EditContactToolStripMenuItem.Name = "EditContactToolStripMenuItem"
+        Me.EditContactToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.EditContactToolStripMenuItem.Text = "Edit Contact..."
+        '
+        'EditBuildingToolStripMenuItem
+        '
+        Me.EditBuildingToolStripMenuItem.Name = "EditBuildingToolStripMenuItem"
+        Me.EditBuildingToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.EditBuildingToolStripMenuItem.Text = "Edit Building..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(712, 511)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Meter Reader Manager - MvvmForms Demo"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewReadingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewContactToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewBuildingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents QuitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditContactToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditBuildingToolStripMenuItem As ToolStripMenuItem
+End Class
