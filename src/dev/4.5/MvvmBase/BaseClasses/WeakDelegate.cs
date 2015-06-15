544aff2add20ca4fe033c25082da416c7884094d
vbspoony <klaus@loeffelmann.de>
2015-06-15 14:40:49 +0200

    Nachzügler: Missing packages.config.

---
+++ src/dev/4.5/MvvmBase/BaseClasses/WeakDelegate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDev.MvvmBaseLib.BaseClasses
+{
+    class WeakDelegate : WeakReference 
+    {
+        private Delegate myWeakDelegate;
+
+        public WeakDelegate(Delegate del):base(del)
+        {
+
+        }
+    }
+}
