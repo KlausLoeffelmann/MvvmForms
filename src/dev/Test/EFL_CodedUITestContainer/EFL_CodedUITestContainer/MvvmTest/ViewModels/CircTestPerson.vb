625ab9345bd54a23c1682d3c72b2d36262dd82ed
GrimDestructor <stephanu@activedevelop.de>
2015-09-25 10:06:25 +0200

    Added Test-ViewModels

---
+++ src/dev/Test/EFL_CodedUITestContainer/EFL_CodedUITestContainer/MvvmTest/ViewModels/CircTestPerson.vb
@@ -0,0 +1,15 @@
+﻿Imports ActiveDevelop.MvvmBaseLib.Mvvm
+
+Public Class CircTestPerson
+    Inherits MvvmViewModelBase
+
+    Property Name As String
+
+    Property Anschrift As CircTestAnschrift
+End Class
+
+Public Class CircTestAnschrift
+    Inherits MvvmViewModelBase
+
+    Property Person As CircTestPerson
+End Class
